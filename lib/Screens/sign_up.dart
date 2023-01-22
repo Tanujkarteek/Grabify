00a714d229cba2e8ebf67f440fb3eec602555d95
Tanujkarteek <allenatanujkarteek@gmail.com>
2023-01-22 15:22:57 +0530

    Some Minor Changes made to the Sign up page

--- lib/Screens/sign_up.dart
+++ lib/Screens/sign_up.dart
@@ -16,103 +16,153 @@ class _MyLoginState extends State<MyLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          SvgPicture.asset(
-            signup,
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (OverscrollIndicatorNotification overscroll) {
+          overscroll.disallowIndicator();
+          return false;
+        },
+        child: SingleChildScrollView(
+          child: Container(
+            child: Stack(
               children: <Widget>[
-                // SizedBox(height: MediaQuery.of(context).size.height * 0.002),
-                // Container(
-                //     height: MediaQuery.of(context).size.height * 0.4,
-                //     width: MediaQuery.of(context).size.width * 0.4,
-                //     child: SizedBox(
-                //         height: 300,
-                //         width: 200,
-                //         child:
-                //             FittedBox(fit: BoxFit.scaleDown, child: Logo()))),
-                Transform.scale(
-                  scale: 0.5,
-                  child: Logo(),
+                SvgPicture.asset(
+                  signup,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                //Take inputs of name and roll number and phonenumber
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Name',
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Roll Number',
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Phone Number',
-                    ),
-                  ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                //button to continue to new password setting page
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      //navigate to sign_up.dart
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => MyLogin()));
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromARGB(255, 244, 119, 52)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      )),
-                      elevation: MaterialStateProperty.all<double>(20),
-                      overlayColor: MaterialStateProperty.all<Color>(
-                          Color.fromARGB(255, 100, 3, 114)),
-                    ),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.28,
-                      height: MediaQuery.of(context).size.height * 0.055,
-                      child: Center(
-                        child: Text(
-                          "Continue",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.07),
+                      // Container(
+                      //     height: MediaQuery.of(context).size.height * 0.4,
+                      //     width: MediaQuery.of(context).size.width * 0.4,
+                      //     child: SizedBox(
+                      //         height: 300,
+                      //         width: 200,
+                      //         child:
+                      //             FittedBox(fit: BoxFit.scaleDown, child: Logo()))),
+                      // Transform.scale(
+                      //   scale: 0.5,
+                      //   child: Logo(),
+                      // )
+                      SizedBox(
+                          height: 175,
+                          child: FittedBox(
+                            child: Logo(),
+                            fit: BoxFit.cover,
+                          )),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.18),
+                      //Take inputs of name and roll number and phonenumber
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 60.0,
+                                  right: 60.0,
+                                  top: 8.0,
+                                  bottom: 8.0,
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Name',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 60.0,
+                                    right: 60.0,
+                                    top: 8.0,
+                                    bottom: 8.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Roll Number',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 60.0,
+                                    right: 60.0,
+                                    top: 8.0,
+                                    bottom: 8.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Phone Number',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  //navigate to sign_up.dart
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MyLogin()));
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Color.fromARGB(255, 224, 75, 1)),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  )),
+                                  elevation:
+                                      MaterialStateProperty.all<double>(20),
+                                  overlayColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Color.fromARGB(255, 100, 3, 114)),
+                                ),
+                                child: SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.28,
+                                  height: MediaQuery.of(context).size.height *
+                                      0.055,
+                                  child: Center(
+                                    child: Text(
+                                      "Continue",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
